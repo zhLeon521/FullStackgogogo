a12a24867b8aa3e9249b6924a019d779b7520235
zhLeon521 <2321837783@qq.com>
2021-05-23 20:49:58 +0800

    Destructuring。三种方法

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,21 +2,31 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-23 20:18:22
+ * @LastEditTime: 2021-05-23 20:49:35
  * @FilePath: \fullstackgogogo\part1\src\App.js
  */
 import React from 'react';
 
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  }
 
-  
+// bornYear函数是Hello函数内部定义的 Component helper functions【组件辅助函数】
+
+const Hello = ({name,age}) => { //终极版：传递给组件的props现在直接解构为变量 name 和 age。
+  // 方法一：解构
+  // const name = props.name;
+  // const age = props.age;
+
+  // 方法2
+  // 表达式 const { name, age } = props 会将值 'Wang Dazhu' 赋值给 name，22赋值给 age。
+  // const { name, age } = props;
+
+  const bornYear = () => new Date().getFullYear() - age; //紧凑写法
+  // 等价于
+  // const bornYear = () => {
+  //   return new Date().getFullYear() - age;
+  // }
   return (
     <div>
-      <p>Hello, {props.name}！！！you are {props.age} years old。</p>
+      <p>Hello, {name}！！！you are {age} years old。</p>
       <p> So, you were probably born in { bornYear() }</p>
   </div>
   )
