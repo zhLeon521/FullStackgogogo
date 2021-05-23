dae9ff904c4ab768dc23a4557af26509adeaec0e
zhLeon521 <2321837783@qq.com>
2021-05-23 18:49:07 +0800

    Multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,22 +2,26 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-23 18:44:00
+ * @LastEditTime: 2021-05-23 18:48:38
  * @FilePath: \fullstackgogogo\part1\src\App.js
  */
 import React from 'react';
 
-const App = () => {
-  console.log('Hello from conponents!');
+const Hello = () => {
+  return <div>
+    <p>Hello, zhLeon521！！</p>
+  </div>
+}
 
-  const now = new Date();
-  const a = 199;
-  const b = 999;
+
+const App = () => {
   return (
     <div>
-      {now.toString()};
-      <p>Hello， zhLeon521！！ </p>
-      {a} plus {b} is {a+b}
+      <h1>FullStack</h1>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
     </div>
   )
 }
