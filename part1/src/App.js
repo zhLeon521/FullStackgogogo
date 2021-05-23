68f212c94302677ac4dfc490b1585ebe7018157e
zhLeon521 <2321837783@qq.com>
2021-05-23 18:45:59 +0800

    components 简单应用

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,15 +2,24 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-23 18:32:50
+ * @LastEditTime: 2021-05-23 18:44:00
  * @FilePath: \fullstackgogogo\part1\src\App.js
  */
 import React from 'react';
 
-const App = () => (
-  <div>
-  <p>Hello， zhLeon521！！</p>
-</div>
-)
+const App = () => {
+  console.log('Hello from conponents!');
+
+  const now = new Date();
+  const a = 199;
+  const b = 999;
+  return (
+    <div>
+      {now.toString()};
+      <p>Hello， zhLeon521！！ </p>
+      {a} plus {b} is {a+b}
+    </div>
+  )
+}
 
 export default App;
