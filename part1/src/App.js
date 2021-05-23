948ba278732a370b6bf9c0d9dbe748e3702702c2
zhLeon521 <2321837783@qq.com>
2021-05-23 18:33:48 +0800

    初始化一个react project

---
+++ part1/src/App.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Autor: Blueheart
+ * @Date: 2021-05-23 16:58:39
+ * @LastEditTime: 2021-05-23 18:32:50
+ * @FilePath: \fullstackgogogo\part1\src\App.js
+ */
+import React from 'react';
+
+const App = () => (
+  <div>
+  <p>Hello， zhLeon521！！</p>
+</div>
+)
+
+export default App;
---
+++ part1/src/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Autor: Blueheart
+ * @Date: 2021-05-23 16:58:39
+ * @LastEditTime: 2021-05-23 17:09:28
+ * @FilePath: \fullstackgogogo\part1\src\index.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
+
