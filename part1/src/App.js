78c5fb429f96ddf53b90409112073ea3ff1eaf43
zhLeon521 <2321837783@qq.com>
2021-05-23 18:53:08 +0800

    props: passing data to components

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,26 +2,28 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-23 18:48:38
+ * @LastEditTime: 2021-05-23 18:52:46
  * @FilePath: \fullstackgogogo\part1\src\App.js
  */
 import React from 'react';
 
-const Hello = () => {
+const Hello = (props) => {
   return <div>
-    <p>Hello, zhLeon521！！</p>
+    <p>Hello, {props.name}！！！you are { props.age} years old。</p>
   </div>
 }
 
 
 const App = () => {
+  const name = "Wang Dazhu";
+  const age = 22;
   return (
     <div>
       <h1>FullStack</h1>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-      <Hello/>
+      <Hello name="zhLeon521" age={ age}/>
+      <Hello name={name} age={ 66+33}/>
+      <Hello name="zhLeon666"/>
+      <Hello name="zhLeon888"/>
     </div>
   )
 }
