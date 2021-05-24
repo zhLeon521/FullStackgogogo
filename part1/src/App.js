7752667b62dc9e99018db80c1c6a2136652678fd
zhLeon521 <2321837783@qq.com>
2021-05-24 19:37:09 +0800

    Stateful component

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,18 +2,25 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-24 19:20:36
+ * @LastEditTime: 2021-05-24 19:33:42
  * @FilePath: \fullstackgogogo\part1\src\App.js
  */
-import React from 'react';
+import React, { useState} from 'react';
 
-const App = (props) => {
-  const { counter } = props
-  return(
-    <div >
-      {counter}
-    </div>
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
   )
+  console.log("rendering……",counter);
+  
+  
+  return (
+    <div>
+      {counter}
+    </div>)
+  
 }
 
 export default App;
--- part1/src/index.js
+++ part1/src/index.js
@@ -2,29 +2,15 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-24 19:26:06
+ * @LastEditTime: 2021-05-24 19:27:51
  * @FilePath: \fullstackgogogo\part1\src\index.js
  */
 import ReactDOM from 'react-dom';
 import App from './App';
 
-let counter = 1;
-
-const refresh = () => {
-  ReactDOM.render(
-    <App counter={counter}/>,
-    document.getElementById('root')
-  );
-}
-
-setInterval(() => {
-  refresh()
-  counter  += 1
-}, 1000)
 
 
-
-
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
 
 
