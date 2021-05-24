d6fc62c8456e9b594df1b0727c256489fe88bc29
zhLeon521 <2321837783@qq.com>
2021-05-24 19:45:52 +0800

    Event handling

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,24 +2,29 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-24 19:33:42
+ * @LastEditTime: 2021-05-24 19:45:24
  * @FilePath: \fullstackgogogo\part1\src\App.js
  */
 import React, { useState} from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  console.log("rendering……",counter);
+  
+  const handleClick = () => { 
+    console.log("Clicked");
+    setCounter(counter + 1);
+
+  }
   
   
   return (
-    <div>
-      {counter}
-    </div>)
+    <>
+      <div> {counter} </div>
+      <button onClick={handleClick}>Plus</button>
+      <button onClick={()=>setCounter(0)}>Zero</button>
+    </>
+
+  )
   
 }
 
