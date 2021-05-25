e3be68fcde412c736ef8005f3ef5d9704d89aad3
zhLeon521 <2321837783@qq.com>
2021-05-25 23:26:08 +0800

    Complex state

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,59 +2,46 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-23 16:58:39
- * @LastEditTime: 2021-05-24 23:57:12
+ * @LastEditTime: 2021-05-25 23:25:19
  * @FilePath: \fullstackgogogo\part1\src\App.js
  */
 import React, { useState} from 'react';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+
+  const [clicks, setClicks] = useState({
+    left:0,right:0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
   
-  const increaseByOne  = () => { 
-    console.log("Clicked increaseByOne");
-    setCounter(counter + 1);
-  }
-
-  const decreaseByOne  = () => { 
-    console.log("Clicked decreaseByOne");
-    setCounter(counter - 1);
-  }
-
-  const setToZero = () => {
-    console.log("Clicked setToZero");
-    setCounter(0)
-  }
-
-  const Display = ({counter}) => {
-    return (
-      <div>
-        {counter}
-      </div>
-    )
-  }
 
 
-  const Button = ({handleClick,text }) => {
-    return (
-      <button onClick={handleClick}>
-        {text}
-      </button>
-    )
 
-  }
+
   
   
   return (
     <>
-      <Display counter={ counter}></Display>
-      <Button handleClick={increaseByOne}
-        text='plus'
-      />
-      <Button handleClick={decreaseByOne}
-        text='minus' />
       
-      <Button handleClick={setToZero}
-      text='zero'/>
+      {clicks.left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Left</button>
+      {clicks.right}
 
     </>
   )
