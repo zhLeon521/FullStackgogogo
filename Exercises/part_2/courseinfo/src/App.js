2e20fa0f0a11aa4ebf527b311d918bcc4d9b1a9a
zhLeon521 <2321837783@qq.com>
2021-06-05 14:33:02 +0800

    新建一个项目

---
+++ Exercises/part_2/courseinfo/src/App.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Autor: Blueheart
+ * @Date: 2021-06-05 14:23:32
+ * @LastEditTime: 2021-06-05 14:26:31
+ * @FilePath: \FullStackgogogo\Exercises\part_2\courseinfo\src\App.js
+ */
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, Wangdazhu.</h1>
+        </div>
+    )
+}
+export default App;
---
+++ Exercises/part_2/courseinfo/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Autor: Blueheart
+ * @Date: 2021-06-05 14:23:41
+ * @LastEditTime: 2021-06-05 14:30:59
+ * @FilePath: \FullStackgogogo\Exercises\part_2\courseinfo\src\index.js
+ */
+
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+    <App />,
+document.getElementById('root'))
